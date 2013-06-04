a3f6d356cd0c7de874c374803103edd329538843
Matthew Landauer <matthew@openaustralia.org>
2013-06-04 19:26:05 +1000

    Moved testing to match where implementation of feed interpretation lives

---
+++ spec/atdis/applications_results_spec.rb
@@ -0,0 +1,35 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe ATDIS::ApplicationsResults do
+  context "paging not supported by vendor" do
+    let(:applications_results) { ATDIS::ApplicationsResults.new("http://www.council.nsw.gov.au/atdis/1.0/applications.json") }
+
+    it "should grab all the applications using json" do
+      # Mock network response
+      RestClient.should_receive(:get).with("http://www.council.nsw.gov.au/atdis/1.0/applications.json").and_return(mock(:to_str => <<-EOF
+{
+  "response": [
+    {
+      "application": {
+        "description": "application1"
+      }     
+    },
+    {
+      "application": {
+        "description": "application2"
+      }      
+    }
+  ]
+}
+      EOF
+      ))
+      application1 = mock("Application")
+      application2 = mock("Application")
+      ATDIS::Application.should_receive(:interpret).with(:description => "application1").and_return(application1)
+      ATDIS::Application.should_receive(:interpret).with(:description => "application2").and_return(application2)
+
+      applications_results.results.should == [application1, application2]
+      applications_results.next.should be_nil
+    end
+  end
+end
--- spec/atdis/applications_spec.rb
+++ spec/atdis/applications_spec.rb
@@ -10,6 +10,14 @@ describe ATDIS::Applications do
     describe "#base_url" do
       it { applications.base_url.should == base_url }
     end
+
+    describe "#all" do
+      it do
+        applications_results = mock
+        ATDIS::ApplicationsResults.should_receive(:new).with(URI.parse("http://www.council.nsw.gov.au/atdis/1.0/applications.json")).and_return(applications_results)
+        applications.all.should == applications_results
+      end
+    end
   end
 
   context "base_url passed as url" do
@@ -17,39 +25,6 @@ describe ATDIS::Applications do
 
     describe "#base_url" do
       it { applications.base_url.should == base_url }
-    end
-  end
-
-  context "paging not supported by vendor" do
-    let(:applications) { ATDIS::Applications.new(base_url_string) }
-
-    it "should grab all the applications using json" do
-      # Mock network response
-      RestClient.should_receive(:get).with(URI.parse("http://www.council.nsw.gov.au/atdis/1.0/applications.json")).and_return(mock(:to_str => <<-EOF
-{
-  "response": [
-    {
-      "application": {
-        "description": "application1"
-      }     
-    },
-    {
-      "application": {
-        "description": "application2"
-      }      
-    }
-  ]
-}
-      EOF
-      ))
-      application1 = mock("Application")
-      application2 = mock("Application")
-      ATDIS::Application.should_receive(:interpret).with(:description => "application1").and_return(application1)
-      ATDIS::Application.should_receive(:interpret).with(:description => "application2").and_return(application2)
-
-      a = applications.all
-      a.results.should == [application1, application2]
-      a.next.should be_nil
     end
   end
 end
