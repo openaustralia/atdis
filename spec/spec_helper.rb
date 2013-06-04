90512ece7127039d9760979cb82dfe9bc1b34ea5
Matthew Landauer <matthew@openaustralia.org>
2013-06-04 14:33:31 +1000

    Add local coverage testing as well as coveralls.io

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,13 @@
+require 'simplecov'
+require 'coveralls'
+
+# Generate coverage locally in html as well as in coveralls.io
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+  SimpleCov::Formatter::HTMLFormatter,
+  Coveralls::SimpleCov::Formatter
+]
+SimpleCov.start
+
 require 'rubygems'
 require 'bundler/setup'
 
