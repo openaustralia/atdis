9a547a0b9b8b31307bd12680fe976f8db1b2b61b
Matthew Landauer <matthew@openaustralia.org>
2013-06-04 18:39:22 +1000

    Base url is converted to URI

--- lib/atdis/applications.rb
+++ lib/atdis/applications.rb
@@ -9,7 +9,7 @@ module ATDIS
     # For example if the base_url is "http://www.council.nsw.gov.au" then the url for listing all the
     # applications is "http://www.council.nsw.gov.au/atdis/1.0/applications.json"
     def initialize(base_url)
-      @base_url = base_url
+      @base_url = base_url.kind_of?(URI) ? base_url : URI.parse(base_url)
     end
 
     def all
--- spec/atdis/applications_spec.rb
+++ spec/atdis/applications_spec.rb
@@ -1,10 +1,31 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe ATDIS::Applications do
+  let (:base_url_string) { "http://www.council.nsw.gov.au" }
+  let (:base_url) { URI.parse(base_url_string) }
+
+  context "base_url passed as string" do
+    let(:applications) { ATDIS::Applications.new(base_url_string) }
+
+    describe "#base_url" do
+      it { applications.base_url.should == base_url }
+    end
+  end
+
+  context "base_url passed as url" do
+    let(:applications) { ATDIS::Applications.new(base_url) }
+
+    describe "#base_url" do
+      it { applications.base_url.should == base_url }
+    end
+  end
+
   context "paging not supported by vendor" do
+    let(:applications) { ATDIS::Applications.new(base_url_string) }
+
     it "should grab all the applications using json" do
       # Mock network response
-      RestClient.should_receive(:get).with("http://www.council.nsw.gov.au/atdis/1.0/applications.json").and_return(mock(:to_str => <<-EOF
+      RestClient.should_receive(:get).with(URI.parse("http://www.council.nsw.gov.au/atdis/1.0/applications.json")).and_return(mock(:to_str => <<-EOF
 {
   "response": [
     {
@@ -26,7 +47,6 @@ describe ATDIS::Applications do
       ATDIS::Application.should_receive(:interpret).with(:description => "application1").and_return(application1)
       ATDIS::Application.should_receive(:interpret).with(:description => "application2").and_return(application2)
 
-      applications = ATDIS::Applications.new("http://www.council.nsw.gov.au")
       applications.all.results.should == [application1, application2]
     end
   end
