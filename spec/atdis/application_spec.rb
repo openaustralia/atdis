06f1d7c2d3dc028eee41390e0d2e1c0706ad7e13
Matthew Landauer <matthew@openaustralia.org>
2013-06-04 12:31:17 +1000

    Test against interpret method

--- spec/atdis/application_spec.rb
+++ spec/atdis/application_spec.rb
@@ -3,31 +3,29 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 describe ATDIS::Application do
   describe "#dat_id" do
     before :each do
-      @application = ATDIS::Application.parse <<-EOF
-{
-  "info": {
-    "dat_id": "DA2013-0381",
-    "description": "New pool plus deck",
-    "authority": "Example Council Shire Council",
-    "lodgement_date": "2013-04-20T02:01:07Z",
-    "determination_date": "2013-06-20T02:01:07Z",
-    "notification_start_date": "2013-04-20T02:01:07Z",
-    "notification_end_date": "2013-05-20T02:01:07Z",
-    "status": "OPEN"
-  },
-  "reference": {
-    "more_info_url": "http://www.examplecouncil.nsw.gov.au/atdis/1.0/applications/DA2013-0381"
-  },
-  "location": {
-    "address": "123 Fourfivesix Street Neutral Bay NSW 2089",
-    "land_title_ref": {
-      "lot": "10",
-      "section": "ABC",
-      "dpsp_id": "DP2013-0381"
-    }
-  }
-}
-      EOF
+      @application = ATDIS::Application.interpret(
+        :info => {
+          :dat_id => "DA2013-0381",
+          :description => "New pool plus deck",
+          :authority => "Example Council Shire Council",
+          :lodgement_date => "2013-04-20T02:01:07Z",
+          :determination_date => "2013-06-20T02:01:07Z",
+          :notification_start_date => "2013-04-20T02:01:07Z",
+          :notification_end_date => "2013-05-20T02:01:07Z",
+          :status => "OPEN"
+        },
+        :reference => {
+          :more_info_url => "http://www.examplecouncil.nsw.gov.au/atdis/1.0/applications/DA2013-0381"
+        },
+        :location => {
+          :address => "123 Fourfivesix Street Neutral Bay NSW 2089",
+          :land_title_ref => {
+            :lot => "10",
+            :section => "ABC",
+            :dpsp_id => "DP2013-0381"
+          }
+        }
+      )
     end
 
     it { @application.dat_id.should == "DA2013-0381" }
