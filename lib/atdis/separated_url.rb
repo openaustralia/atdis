0c14a04714094f86ee90fef1a5e383bd0e93223a
Matthew Landauer <matthew@openaustralia.org>
2013-06-14 18:26:46 +1000

    Only store the full url now

--- lib/atdis/separated_url.rb
+++ lib/atdis/separated_url.rb
@@ -1,26 +1,38 @@
 module ATDIS
   class SeparatedURL
-    attr_accessor :url, :url_params
+    attr_accessor :full_url
 
-    def initialize(url, url_params = {})
-      @url, @url_params = url, url_params
-      url_params.each_key do |k|
-        url_params[k] = url_params[k].to_s
+    def initialize(url, url_params)
+      @full_url = SeparatedURL.full_url(url, url_params)
+    end
+
+    def url
+      SeparatedURL.parse_url(full_url)
+    end
+
+    def url_params
+      SeparatedURL.parse_url_params(full_url)
+    end
+
+    def self.parse_url(full_url)
+      uri = URI.parse(full_url)
+      "#{uri.scheme}://#{uri.host}#{uri.path}"
+    end
+
+    def self.parse_url_params(full_url)
+      uri = URI.parse(full_url)
+      if uri.query
+        Hash[*CGI::parse(uri.query).map{|k,v| [k.to_sym,v.first]}.flatten]
+      else
+        {}
       end
     end
 
     def self.parse(full_url)
-      uri = URI.parse(full_url)
-      url = "#{uri.scheme}://#{uri.host}#{uri.path}"
-      if uri.query
-        url_params = Hash[*CGI::parse(uri.query).map{|k,v| [k.to_sym,v.first]}.flatten]
-      else
-        url_params = {}
-      end
-      SeparatedURL.new(url, url_params)
+      SeparatedURL.new(parse_url(full_url), parse_url_params(full_url))
     end
 
-    def full_url
+    def self.full_url(url, url_params)
       # TODO Correctly encode url_params
       # Doing this jiggery pokery to ensure the params are sorted alphabetically (even on Ruby 1.8)
       query = url_params.map{|k,v| [k.to_s, v]}.sort.map{|k,v| "#{k}=#{v}"}.join("&")
