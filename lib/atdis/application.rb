db0e35c7ffdcaaf84fde9e1e81131f9a060861ea
Matthew Landauer <matthew@openaustralia.org>
2013-06-17 17:44:48 +1000

    Separate mapping from conversion

--- lib/atdis/application.rb
+++ lib/atdis/application.rb
@@ -6,33 +6,32 @@ module ATDIS
       :lodgement_date, :determination_date, :status, :notification_start_date, :notification_end_date,
       :officer, :estimated_cost, :more_info_url, :comments_url, :location) do
 
+    def self.create(params)
+      new(*members.map{|m| params[m.to_sym]})
+    end
+
     def self.parse(text)
       interpret(MultiJson.load(text, :symbolize_keys => true))
     end
 
     def self.interpret(data)
-      if data[:info]
-        dat_id = data[:info][:dat_id]
-        last_modified_date = DateTime.parse(data[:info][:last_modified_date]) if data[:info][:last_modified_date]
-        description = data[:info][:description]
-        authority = data[:info][:authority]
-        lodgement_date = DateTime.parse(data[:info][:lodgement_date]) if data[:info][:lodgement_date]
-        determination_date = DateTime.parse(data[:info][:determination_date]) if data[:info][:determination_date]
-        status = data[:info][:status]
-        notification_start_date = DateTime.parse(data[:info][:notification_start_date]) if data[:info][:notification_start_date]
-        notification_end_date = DateTime.parse(data[:info][:notification_end_date]) if data[:info][:notification_end_date]
-        officer = data[:info][:officer]
-        estimated_cost = data[:info][:estimated_cost]
-      end
-      if data[:reference]
-        more_info_url = URI.parse(data[:reference][:more_info_url]) if data[:reference][:more_info_url]
-        comments_url = URI.parse(data[:reference][:comments_url]) if data[:reference][:comments_url]
-      end
-      location = Location.interpret(data[:location]) if data[:location]
+      values = {}
+      # Map json structure to our values
+      values = values.merge(data[:info]) if data[:info]
+      values = values.merge(data[:reference]) if data[:reference]
+      values[:location] = data[:location]
 
-      Application.new(dat_id, last_modified_date, description, authority,
-        lodgement_date, determination_date, status, notification_start_date, notification_end_date,
-        officer, estimated_cost, more_info_url, comments_url, location)
+      # Convert values (if required)
+      values[:last_modified_date] = DateTime.parse(values[:last_modified_date]) if values[:last_modified_date]
+      values[:lodgement_date] = DateTime.parse(values[:lodgement_date]) if values[:lodgement_date]
+      values[:determination_date] = DateTime.parse(values[:determination_date]) if values[:determination_date]
+      values[:notification_start_date] = DateTime.parse(values[:notification_start_date]) if values[:notification_start_date]
+      values[:notification_end_date] = DateTime.parse(values[:notification_end_date]) if values[:notification_end_date]
+      values[:more_info_url] = URI.parse(values[:more_info_url]) if values[:more_info_url]
+      values[:comments_url] = URI.parse(values[:comments_url]) if values[:comments_url]
+      values[:location] = Location.interpret(values[:location]) if values[:location]
+
+      Application.create(values)
     end
   end
 end
