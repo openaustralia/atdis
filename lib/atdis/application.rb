31259fd34d38279559966f8d95a7f33bf4ba4b63
Matthew Landauer <matthew@openaustralia.org>
2013-06-17 16:40:08 +1000

    Round out a bit more of the implementation of application

--- lib/atdis/application.rb
+++ lib/atdis/application.rb
@@ -3,9 +3,9 @@ require 'date'
 
 module ATDIS
   class Application
-    attr_accessor :dat_id, :description, :authority,
-      :lodgement_date, :determination_date, :notification_start_date, :notification_end_date,
-      :status, :more_info_url, :location
+    attr_accessor :dat_id, :last_modified_date, :description, :authority,
+      :lodgement_date, :determination_date, :status, :notification_start_date, :notification_end_date,
+      :officer, :estimated_cost, :more_info_url, :location
 
     def self.parse(text)
       interpret(MultiJson.load(text, :symbolize_keys => true))
@@ -16,13 +16,16 @@ module ATDIS
       
       if data[:info]
         a.dat_id = data[:info][:dat_id]
+        a.last_modified_date = DateTime.parse(data[:info][:last_modified_date]) if data[:info][:last_modified_date]
         a.description = data[:info][:description]
         a.authority = data[:info][:authority]
         a.lodgement_date = DateTime.parse(data[:info][:lodgement_date]) if data[:info][:lodgement_date]
         a.determination_date = DateTime.parse(data[:info][:determination_date]) if data[:info][:determination_date]
+        a.status = data[:info][:status]
         a.notification_start_date = DateTime.parse(data[:info][:notification_start_date]) if data[:info][:notification_start_date]
         a.notification_end_date = DateTime.parse(data[:info][:notification_end_date]) if data[:info][:notification_end_date]
-        a.status = data[:info][:status]
+        a.officer = data[:info][:officer]
+        a.estimated_cost = data[:info][:estimated_cost]
       end
       a.more_info_url = data[:reference][:more_info_url] if data[:reference]
       a.location = Location.interpret(data[:location]) if data[:location]
--- spec/atdis/application_spec.rb
+++ spec/atdis/application_spec.rb
@@ -13,13 +13,18 @@ describe ATDIS::Application do
     @application = ATDIS::Application.interpret(
       :info => {
         :dat_id => "DA2013-0381",
+        :last_modified_date => "2013-04-20T02:01:07Z",
         :description => "New pool plus deck",
         :authority => "Example Council Shire Council",
         :lodgement_date => "2013-04-20T02:01:07Z",
-        :determination_date => "2013-06-20T02:01:07Z",
+        :determination_date => "2013-06-20",
+        :status => "OPEN",
         :notification_start_date => "2013-04-20T02:01:07Z",
         :notification_end_date => "2013-05-20T02:01:07Z",
-        :status => "OPEN"
+        :officer => "Ms Smith",
+        # TODO: In ATDIS-1.0.3 it does not specify whether this is a float or a string and whether to include (or not) AUD or dollar sign
+        # For the time being we'll just assume it's a free-form string
+        :estimated_cost => "50,000"
       },
       :reference => {
         :more_info_url => "http://www.examplecouncil.nsw.gov.au/atdis/1.0/applications/DA2013-0381"
@@ -36,12 +41,15 @@ describe ATDIS::Application do
   end
 
   it { @application.dat_id.should == "DA2013-0381" }
+  it { @application.last_modified_date.should == DateTime.new(2013,4,20,2,1,7) }
   it { @application.description.should == "New pool plus deck" }
   it { @application.authority.should == "Example Council Shire Council" }
   it { @application.lodgement_date.should == DateTime.new(2013,4,20,2,1,7) }
-  it { @application.determination_date.should == DateTime.new(2013,6,20,2,1,7) }
+  it { @application.determination_date.should == DateTime.new(2013,6,20) }
   it { @application.notification_start_date.should == DateTime.new(2013,4,20,2,1,7) }
   it { @application.notification_end_date.should == DateTime.new(2013,5,20,2,1,7) }
+  it { @application.officer.should == "Ms Smith" }
+  it { @application.estimated_cost.should == "50,000"}
   it { @application.status.should == "OPEN" }
   it { @application.more_info_url.should == "http://www.examplecouncil.nsw.gov.au/atdis/1.0/applications/DA2013-0381" }
   it { @application.location.should == @location }
