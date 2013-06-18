d1d5b480499e572a6aeb8fc0900fc17f9eb8a885
Matthew Landauer <matthew@openaustralia.org>
2013-06-18 18:30:12 +1000

    Event now correctly processes things

--- lib/atdis/event.rb
+++ lib/atdis/event.rb
@@ -1,4 +1,12 @@
 module ATDIS
-  class Event
+  Event = Struct.new(:id, :date, :description, :event_type, :status) do
+    def self.interpret(data)
+      values = data
+
+      # Convert values (if required)
+      values[:date] = DateTime.parse(values[:date]) if values[:date]
+
+      Event.new(*members.map{|m| values[m.to_sym]})
+    end
   end
 end
---
+++ spec/atdis/event_spec.rb
@@ -0,0 +1,25 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe ATDIS::Event do
+  it ".id" do
+    ATDIS::Event.interpret(:id => "27B/6").id.should == "27B/6"
+  end
+
+  it ".date" do
+    ATDIS::Event.interpret(:date => "2013-06-18").date.should == DateTime.new(2013,6,18)
+  end
+
+  it ".description" do
+    ATDIS::Event.interpret(:description => "A very fine event").description.should == "A very fine event"
+  end
+
+  it ".event_type" do
+    # TODO Is event_type always a string? ATDIS-1.0.3 doesn't say
+    ATDIS::Event.interpret(:event_type => "approval").event_type.should == "approval"
+  end
+
+  it ".status" do
+    # TODO Is status always a string? ATDIS-1.0.3 doesn't say
+    ATDIS::Event.interpret(:status => "approved").status.should == "approved"
+  end
+end
