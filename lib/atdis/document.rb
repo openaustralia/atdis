e19b5d3aab5eadd9191912fa4d734ddb94ec3729
Matthew Landauer <matthew@openaustralia.org>
2013-06-18 19:04:51 +1000

    Implementation of Document

--- lib/atdis/document.rb
+++ lib/atdis/document.rb
@@ -1,4 +1,9 @@
 module ATDIS
-  class Document
+  Document = Struct.new(:ref, :title, :document_url) do
+    def self.interpret(data)
+      data[:document_url] = URI.parse(data[:document_url]) if data[:document_url]
+      
+      Document.new(*members.map{|m| data[m.to_sym]})
+    end
   end
 end
---
+++ spec/atdis/document_spec.rb
@@ -0,0 +1,15 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe ATDIS::Document do
+  it ".ref" do
+    ATDIS::Document.interpret(:ref => "27B/6").ref.should == "27B/6"
+  end
+
+  it ".title" do
+    ATDIS::Document.interpret(:title => "Authorisation for Repairs").title.should == "Authorisation for Repairs"
+  end
+
+  it ".document_url" do
+    ATDIS::Document.interpret(:document_url => "http://foo.com/bar").document_url.should == URI.parse("http://foo.com/bar")
+  end
+end
