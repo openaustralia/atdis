2f86d3e80d1b35e4a0e010d850fd909019962698
Matthew Landauer <matthew@openaustralia.org>
2013-06-18 19:19:06 +1000

    Implementation of Person

--- lib/atdis/person.rb
+++ lib/atdis/person.rb
@@ -1,4 +1,7 @@
 module ATDIS
-  class Person
+  Person = Struct.new(:name, :role, :contact) do
+    def self.interpret(data)
+      Person.new(*members.map{|m| data[m.to_sym]})
+    end
   end
 end
---
+++ spec/atdis/person_spec.rb
@@ -0,0 +1,15 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe ATDIS::Person do
+  it ".name" do
+    ATDIS::Person.interpret(:name => "Tuttle").name.should == "Tuttle"
+  end
+
+  it ".role" do
+    ATDIS::Person.interpret(:role => "Heating Engineer").role.should == "Heating Engineer"
+  end
+
+  it ".contact" do
+    ATDIS::Person.interpret(:contact => "94-FLUSH").contact.should == "94-FLUSH"
+  end
+end
