7aa8960d63b84e9ca1bd64ec1bcc280a2559339c
Matthew Landauer <matthew@openaustralia.org>
2013-06-19 11:14:08 +1000

    Restructured tests of application a little

--- spec/atdis/application_spec.rb
+++ spec/atdis/application_spec.rb
@@ -1,79 +1,88 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe ATDIS::Application do
-  before :each do
-    @location = mock("Location")
-    ATDIS::Location.should_receive(:interpret).with(
-      :address => "123 Fourfivesix Street Neutral Bay NSW 2089",
-      :land_title_ref => {
-        :lot => "10",
-        :section => "ABC",
-        :dpsp_id => "DP2013-0381"
-      }).and_return(@location)
-    @application = ATDIS::Application.interpret(
-      :info => {
-        :dat_id => "DA2013-0381",
-        :last_modified_date => "2013-04-20T02:01:07Z",
-        :description => "New pool plus deck",
-        :authority => "Example Council Shire Council",
-        :lodgement_date => "2013-04-20T02:01:07Z",
-        :determination_date => "2013-06-20",
-        :status => "OPEN",
-        :notification_start_date => "2013-04-20T02:01:07Z",
-        :notification_end_date => "2013-05-20T02:01:07Z",
-        :officer => "Ms Smith",
-        # TODO: In ATDIS-1.0.3 it does not specify whether this is a float or a string and whether to include (or not) AUD or dollar sign
-        # For the time being we'll just assume it's a free-form string
-        :estimated_cost => "50,000"
-      },
-      :reference => {
-        :more_info_url => "http://www.examplecouncil.nsw.gov.au/atdis/1.0/applications/DA2013-0381",
-        :comments_url => "http://www.examplecouncil.nsw.gov.au/atdis/1.0/applications/DA2013-0381/comment"
-      },
-      :location => {
-        :address => "123 Fourfivesix Street Neutral Bay NSW 2089",
-        :land_title_ref => {
-          :lot => "10",
-          :section => "ABC",
-          :dpsp_id => "DP2013-0381"
-        }
-      }
-    )
+
+  it ".dat_id" do
+    ATDIS::Application.interpret(:info => {:dat_id => "DA2013-0381"}).dat_id.should == "DA2013-0381"
   end
 
-  it { @application.dat_id.should == "DA2013-0381" }
-  it { @application.last_modified_date.should == DateTime.new(2013,4,20,2,1,7) }
-  it { @application.description.should == "New pool plus deck" }
-  it { @application.authority.should == "Example Council Shire Council" }
-  it { @application.lodgement_date.should == DateTime.new(2013,4,20,2,1,7) }
-  it { @application.determination_date.should == DateTime.new(2013,6,20) }
-  it { @application.notification_start_date.should == DateTime.new(2013,4,20,2,1,7) }
-  it { @application.notification_end_date.should == DateTime.new(2013,5,20,2,1,7) }
-  it { @application.officer.should == "Ms Smith" }
-  it { @application.estimated_cost.should == "50,000"}
-  it { @application.status.should == "OPEN" }
-  it { @application.more_info_url.should == URI.parse("http://www.examplecouncil.nsw.gov.au/atdis/1.0/applications/DA2013-0381") }
-  it { @application.comments_url.should == URI.parse("http://www.examplecouncil.nsw.gov.au/atdis/1.0/applications/DA2013-0381/comment") }
-  it { @application.location.should == @location }
+  it ".last_modified_date" do
+    ATDIS::Application.interpret(:info => {:last_modified_date => "2013-04-20T02:01:07Z"}).last_modified_date.should == DateTime.new(2013,4,20,2,1,7)
+  end
 
-  describe "events" do
-    it "should pass responsibility for interpreting an event" do
-      event1 = mock
-      event2 = mock
-      ATDIS::Event.should_receive(:interpret).with(:id => "event1").and_return(event1)
-      ATDIS::Event.should_receive(:interpret).with(:id => "event2").and_return(event2)
-      application = ATDIS::Application.interpret(
-        :events => [
-          {
-            :id => "event1"
-          },
-          {
-            :id => "event2"
-          }
-        ]
-      )
-      application.events.should == [event1, event2]
-    end
+  it ".description" do
+    ATDIS::Application.interpret(:info => {:description => "New pool plus deck"}).description.should == "New pool plus deck"
+  end
+
+  it ".authority" do
+    ATDIS::Application.interpret(:info => {:authority => "Example Council Shire Council"}).authority.should == "Example Council Shire Council"
+  end
+
+  it ".lodgement_date" do
+    ATDIS::Application.interpret(:info => {:lodgement_date => "2013-04-20T02:01:07Z"}).lodgement_date.should == DateTime.new(2013,4,20,2,1,7)
+  end
+
+  it ".determination_date" do
+    ATDIS::Application.interpret(:info => {:determination_date => "2013-06-20"}).determination_date.should == DateTime.new(2013,6,20)
+  end
+
+  it ".notification_start_date" do
+    ATDIS::Application.interpret(:info => {:notification_start_date => "2013-04-20T02:01:07Z"}).notification_start_date.should == DateTime.new(2013,4,20,2,1,7)
+  end
+
+  it "notification_end_date" do
+    ATDIS::Application.interpret(:info => {:notification_end_date => "2013-05-20T02:01:07Z"}).notification_end_date.should == DateTime.new(2013,5,20,2,1,7)
+  end
+
+  it ".officer" do
+    ATDIS::Application.interpret(:info => {:officer => "Ms Smith"}).officer.should == "Ms Smith"
+  end
+
+  it ".estimated_cost" do
+    # TODO: In ATDIS-1.0.3 it does not specify whether this is a float or a string and whether to include (or not) AUD or dollar sign
+    # For the time being we'll just assume it's a free-form string
+    ATDIS::Application.interpret(:info => {:estimated_cost => "50,000"}).estimated_cost.should == "50,000"
+  end
+
+  it ".status" do
+    ATDIS::Application.interpret(:info => {:status => "OPEN"}).status.should == "OPEN"
+  end
+
+  it ".more_info_url" do
+    ATDIS::Application.interpret(:reference => {:more_info_url => "http://foo.com/bar"}).more_info_url.should == URI.parse("http://foo.com/bar")
+  end
+
+  it "comments_url" do
+    ATDIS::Application.interpret(:reference => {:comments_url => "http://foo.com/comment"}).comments_url.should == URI.parse("http://foo.com/comment")
+  end
+
+  it ".location" do
+    location = mock
+    ATDIS::Location.should_receive(:interpret).with(:address => "123 Fourfivesix Street").and_return(location)
+    application = ATDIS::Application.interpret(
+      :location => {
+        :address => "123 Fourfivesix Street"
+      }
+    )
+    application.location.should == location
+  end
+
+  it ".events" do
+    event1 = mock
+    event2 = mock
+    ATDIS::Event.should_receive(:interpret).with(:id => "event1").and_return(event1)
+    ATDIS::Event.should_receive(:interpret).with(:id => "event2").and_return(event2)
+    application = ATDIS::Application.interpret(
+      :events => [
+        {
+          :id => "event1"
+        },
+        {
+          :id => "event2"
+        }
+      ]
+    )
+    application.events.should == [event1, event2]
   end
 
   it ".documents" do
