d2981fd5e610ef75cf92b7e604c9038379bc3ed6
Matthew Landauer <matthew@openaustralia.org>
2013-08-29 13:27:49 +1000

    Much stricter implementation of iso 8601 parsing

--- lib/atdis/model.rb
+++ lib/atdis/model.rb
@@ -23,7 +23,16 @@ module ATDIS
     # TODO We're currently far more forgiving here then we should be. It will accept all
     # kinds of different date formats. Tighten this up only accept iso8601.
     def self.iso8601(text)
-      DateTime.parse(text)
+      # This would be much easier if we knew we only had to support Ruby 1.9 or greater because it has
+      # an implementation built in. Because for the time being we need to support Ruby 1.8 as well
+      # we'll build an implementation of parsing by hand. Ugh.
+      # Referencing http://www.w3.org/TR/NOTE-datetime
+      # In section 4.3.1 of ATDIS 1.0.4 it shows two variants of iso 8601, either the full date
+      # or the full date with hours, seconds, minutes and timezone. We'll assume that these
+      # are the two variants that are allowed.
+      if text.match(/^\d\d\d\d-\d\d-\d\d(T\d\d:\d\d:\d\d(Z|(\+|-)\d\d:\d\d))?$/)
+        DateTime.parse(text)
+      end
     end
   end
 end
---
+++ spec/atdis/model_spec.rb
@@ -0,0 +1,12 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe ATDIS::Model do
+  describe ".iso8601" do 
+    it {ATDIS::Model.iso8601("2013-04-20T02:01:07Z").should == DateTime.new(2013,4,20,2,1,7)}
+    it {ATDIS::Model.iso8601("2013-04-20").should == DateTime.new(2013,4,20)}
+    it {ATDIS::Model.iso8601("2013-04-20T02:01:07+05:00").should == DateTime.new(2013,4,20,2,1,7,"+5")}
+    it {ATDIS::Model.iso8601("2013-04-20T02:01:07-05:00").should == DateTime.new(2013,4,20,2,1,7,"-5")}
+    it {ATDIS::Model.iso8601("2013-04").should be_nil}
+    it {ATDIS::Model.iso8601("18 September 2013").should be_nil}
+  end
+end
