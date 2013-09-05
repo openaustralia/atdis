c3ce6950bc863c71abe2b2dac8534aedbf232f35
Matthew Landauer <matthew@openaustralia.org>
2013-09-06 08:43:16 +1000

    Reduce duplication of checking for no casting required

--- spec/atdis/model_spec.rb
+++ spec/atdis/model_spec.rb
@@ -1,14 +1,14 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe ATDIS::Model do
-  describe ".cast_datetime" do 
-    it {ATDIS::Model.cast_datetime("2013-04-20T02:01:07Z").should == DateTime.new(2013,4,20,2,1,7)}
-    it {ATDIS::Model.cast_datetime("2013-04-20").should == DateTime.new(2013,4,20)}
-    it {ATDIS::Model.cast_datetime("2013-04-20T02:01:07+05:00").should == DateTime.new(2013,4,20,2,1,7,"+5")}
-    it {ATDIS::Model.cast_datetime("2013-04-20T02:01:07-05:00").should == DateTime.new(2013,4,20,2,1,7,"-5")}
-    it {ATDIS::Model.cast_datetime("2013-04").should be_nil}
-    it {ATDIS::Model.cast_datetime("18 September 2013").should be_nil}
-    it {ATDIS::Model.cast_datetime(DateTime.new(2013,4,20,2,1,7)).should == DateTime.new(2013,4,20,2,1,7)}
+  describe ".cast" do 
+    it {ATDIS::Model.cast("2013-04-20T02:01:07Z", DateTime).should == DateTime.new(2013,4,20,2,1,7)}
+    it {ATDIS::Model.cast("2013-04-20", DateTime).should == DateTime.new(2013,4,20)}
+    it {ATDIS::Model.cast("2013-04-20T02:01:07+05:00", DateTime).should == DateTime.new(2013,4,20,2,1,7,"+5")}
+    it {ATDIS::Model.cast("2013-04-20T02:01:07-05:00", DateTime).should == DateTime.new(2013,4,20,2,1,7,"-5")}
+    it {ATDIS::Model.cast("2013-04", DateTime).should be_nil}
+    it {ATDIS::Model.cast("18 September 2013", DateTime).should be_nil}
+    it {ATDIS::Model.cast(DateTime.new(2013,4,20,2,1,7), DateTime).should == DateTime.new(2013,4,20,2,1,7)}
   end
 
   describe ".map_fields" do
