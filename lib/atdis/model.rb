a3512716614b8b813539e3f5e2e0aa60fce7bc92
Matthew Landauer <matthew@openaustralia.org>
2013-09-05 19:21:05 +1000

    Rename method

--- lib/atdis/model.rb
+++ lib/atdis/model.rb
@@ -57,12 +57,12 @@ module ATDIS
     end
 
     # Map json structure to our values
-    def self.map_fields2(valid_fields, data)
+    def self.map_fields(valid_fields, data)
       values = {:json_left_overs => {}}
       data.each_key do |key|
         if valid_fields[key]
           if valid_fields[key].kind_of?(Hash)
-            v2 = map_fields2(valid_fields[key], data[key])
+            v2 = map_fields(valid_fields[key], data[key])
             l2 = v2.delete(:json_left_overs)
             values = values.merge(v2)
             values[:json_left_overs][key] = l2 unless l2.empty?
@@ -77,7 +77,7 @@ module ATDIS
     end
 
     def self.convert(data)
-      map_fields2(valid_fields, data)
+      map_fields(valid_fields, data)
     end
 
     def self.cast(value, type)
