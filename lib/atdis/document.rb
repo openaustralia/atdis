54d4d6cbb8ebbb17f2afa36ad6f9cb133aeef0e0
Matthew Landauer <matthew@openaustralia.org>
2013-09-06 16:11:16 +1000

    Add validations to Document

--- lib/atdis/document.rb
+++ lib/atdis/document.rb
@@ -3,5 +3,10 @@ module ATDIS
     field_mappings :ref => [:ref, String],
       :title => [:title, String],
       :document_url => [:document_url, URI]
+
+    # Mandatory parameters
+    validates :ref, :title, :document_url, :presence_before_type_cast => true
+    # Other validations
+    validates :document_url, :url => true
   end
 end
