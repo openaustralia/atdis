81afb53c2fbd5692f925e232d7b665388da74135
Matthew Landauer <matthew@openaustralia.org>
2013-09-26 10:58:38 +1000

    Extract methods

--- lib/atdis/feed.rb
+++ lib/atdis/feed.rb
@@ -4,6 +4,8 @@ module ATDIS
   class Feed
     attr_reader :base_url
 
+    VALID_OPTIONS = [:page, :postcode, :lodgement_date_start, :lodgement_date_end, :last_modified_date_start, :last_modified_date_end]
+
     # base_url - the base url from which the urls for all atdis urls are made
     # It should be of the form:
     # http://www.council.nsw.gov.au/atdis/1.0/applications.json
@@ -12,18 +14,14 @@ module ATDIS
     end
 
     def url(options = {})
-      valid_options = [:page, :postcode, :lodgement_date_start, :lodgement_date_end, :last_modified_date_start, :last_modified_date_end]
-      invalid_options = options.keys - valid_options
+      invalid_options = options.keys - VALID_OPTIONS
       if !invalid_options.empty?
         raise "Unexpected options used: #{invalid_options.join(',')}"
       end
       options[:postcode] = options[:postcode].join(",") if options[:postcode].respond_to?(:join)
 
-      url = base_url
-      unless options.empty?
-        url += "?" + options.sort{|a,b| a.first.to_s <=> b.first.to_s}.map{|k,v| "#{k}=#{v}"}.join("&")
-      end
-      url
+      q = Feed.options_to_query(options)  
+      q == "" ? base_url : "#{base_url}?#{q}"
     end
 
     def self.base_url_from_url(url)
@@ -35,13 +33,7 @@ module ATDIS
 
     def self.options_from_url(url)
       u = URI.parse(url)
-      options = {}
-      if u.query
-        u.query.split("&").each do |t|
-          key, value = t.split("=")
-          options[key.to_sym] = value
-        end
-      end
+      options = query_to_options(u.query)
       [:lodgement_date_start, :lodgement_date_end, :last_modified_date_start, :last_modified_date_end].each do |k|
         options[k] = Date.parse(options[k]) if options[k]
       end
@@ -51,6 +43,26 @@ module ATDIS
 
     def applications(options = {})
       Page.read_url(url(options))
+    end
+
+    private
+
+    # Turn a query string of the form "foo=bar&hello=sir" to {:foo => "bar", :hello => "sir"}
+    def self.query_to_options(query)
+      options = {}
+      if query
+        query.split("&").each do |t|
+          key, value = t.split("=")
+          options[key.to_sym] = value
+        end
+      end
+      options
+    end
+
+    # Turn an options hash of the form {:foo => "bar", :hello => "sir"} into a query
+    # string of the form "foo=bar&hello=sir"
+    def self.options_to_query(options)
+      options.sort{|a,b| a.first.to_s <=> b.first.to_s}.map{|k,v| "#{k}=#{v}"}.join("&")
     end
   end
 end
