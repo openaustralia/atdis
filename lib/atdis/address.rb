b08b0dda237d2988f1efeca177a8eb902d1d3fee
Matthew Landauer <matthew@openaustralia.org>
2014-03-20 16:38:27 +1100

    Make all address fields mandatory

--- lib/atdis/address.rb
+++ lib/atdis/address.rb
@@ -6,6 +6,9 @@ module ATDIS
       postcode: String
     })
 
+    # Mandatory parameters
+    validates :street, :suburb, :postcode, presence_before_type_cast: {spec_section: "4.3.3"}
+
     validates :postcode, format: { with: /\A[0-9]{4}\z/, message: ATDIS::ErrorMessage.new("is not a valid postcode", "4.3.3")}
   end
 end
--- spec/atdis/address_spec.rb
+++ spec/atdis/address_spec.rb
@@ -1,16 +1,21 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe ATDIS::Address do
-  context "valid postcode" do
-    let(:a) { ATDIS::Address.new(postcode: "2780")}
-    it { a.should be_valid }
-  end
+  context "valid address" do
+    let(:a) { ATDIS::Address.new(
+      street: "123 Fourfivesix Street",
+      suburb: "Neutral Bay",
+      postcode: "2780"
+    )}
 
-  context "postcode that is too short" do
-    let(:a) { ATDIS::Address.new(postcode: "278")}
-    it {
-      a.should_not be_valid
-      a.errors.messages.should == {postcode: [ATDIS::ErrorMessage.new("is not a valid postcode", "4.3.3")]}
-    }
+    it { a.should be_valid }
+
+    context "postcode that is too short" do
+      before(:each) { a.postcode = "278" }
+      it {
+        a.should_not be_valid
+        a.errors.messages.should == {postcode: [ATDIS::ErrorMessage.new("is not a valid postcode", "4.3.3")]}
+      }
+    end
   end
 end
