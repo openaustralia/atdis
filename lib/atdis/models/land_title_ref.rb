d9a47c1f456a8ff15fe72db313e1ffe533d0ce67
Matthew Landauer <matthew@openaustralia.org>
2014-03-20 17:58:43 +1100

    Validate torrens and other presence

--- lib/atdis/models/land_title_ref.rb
+++ lib/atdis/models/land_title_ref.rb
@@ -4,11 +4,23 @@ module ATDIS
   module Models
     class LandTitleRef < Model
       set_field_mappings ({
-        torrens: TorrensTitle
+        torrens: TorrensTitle,
+        other:   Hash
       })
 
       # This model is only valid if the children are valid
       validates :torrens, valid: true
+
+      validate :check_title_presence
+
+      def check_title_presence
+        if torrens.nil? && other.nil?
+          errors.add(:torrens, ATDIS::ErrorMessage.new("or other needs be present", "4.3.3"))
+        end
+        if torrens && other
+          errors.add(:torrens, ATDIS::ErrorMessage.new("and other can't both be present", "4.3.3"))
+        end
+      end
     end
   end
 end
--- spec/atdis/models/land_title_ref_spec.rb
+++ spec/atdis/models/land_title_ref_spec.rb
@@ -1,4 +1,39 @@
 require "spec_helper"
 
 describe ATDIS::Models::LandTitleRef do
+  context "torrens" do
+    before(:each) {
+      m = double
+      ATDIS::Models::TorrensTitle.should_receive(:interpret).with({lot: "10"}).and_return(m)
+      m.should_receive(:valid?).and_return(true)
+    }
+    let(:l) { ATDIS::Models::LandTitleRef.new(torrens: {lot: "10"}) }
+    it { l.should be_valid }
+  end
+
+  context "other" do
+    let(:l) { ATDIS::Models::LandTitleRef.new(other: {some: "foo", random: "stuff"})}
+    it { l.should be_valid }
+  end
+
+  context "no torrens or other" do
+    let(:l) { ATDIS::Models::LandTitleRef.new }
+    it {
+      l.should_not be_valid
+      l.errors.messages.should == {torrens: [ATDIS::ErrorMessage.new("or other needs be present", "4.3.3")]}
+    }
+  end
+
+  context "both torrens and other" do
+    before(:each) {
+      m = double
+      ATDIS::Models::TorrensTitle.should_receive(:interpret).with({lot: "10"}).and_return(m)
+      m.should_receive(:valid?).and_return(true)
+    }
+    let(:l) { ATDIS::Models::LandTitleRef.new(torrens: {lot: "10"}, other: {some: "foo", random: "stuff"})}
+    it {
+      l.should_not be_valid
+      l.errors.messages.should == {torrens: [ATDIS::ErrorMessage.new("and other can't both be present", "4.3.3")]}
+    }
+  end
 end
