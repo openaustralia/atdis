d59ef61a318209eabe9b234d63fe7fd36ad230dc
Matthew Landauer <matthew@openaustralia.org>
2014-03-20 15:39:40 +1100

    Remove old comment

--- lib/atdis/response.rb
+++ lib/atdis/response.rb
@@ -2,8 +2,6 @@ require "atdis/application"
 
 module ATDIS
   class Response < Model
-    # TODO When we remove support for Ruby 1.8 we can convert field_mappings back to a hash
-    # which is much more readable
     set_field_mappings ({
       application: Application
     })
