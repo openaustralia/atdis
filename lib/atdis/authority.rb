a494f1a1dc08ff1262e2888072f8381b9a2df297
Matthew Landauer <matthew@openaustralia.org>
2014-03-20 13:00:21 +1100

    Validate Authority#ref

--- lib/atdis/authority.rb
+++ lib/atdis/authority.rb
@@ -8,5 +8,8 @@ module ATDIS
     # ref is a "Unique Authority Identifier" and should have the form http://www.council.nsw.gov.au/atdis/1.0
     # It should also be consistent for each council.
     # TODO: Check somehow that the authority ref is consistently used
+
+    validates :ref, http_url: {spec_section: "4.2"}
+    validates :ref, format: { with: /atdis\/1.0\z/, message: ATDIS::ErrorMessage.new("is not a valid Unique Authority Identifier", "4.2")}
   end
 end
---
+++ spec/atdis/authority_spec.rb
@@ -0,0 +1,34 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe ATDIS::Authority do
+  describe "validations" do
+    context "a valid ref" do
+      let(:a) { ATDIS::Authority.new(ref: "http://www.council.nsw.gov.au/atdis/1.0")}
+      it {a.should be_valid}
+    end
+
+    context "a valid ref with https" do
+      let(:a) { ATDIS::Authority.new(ref: "https://www.council.nsw.gov.au/atdis/1.0")}
+      it {a.should be_valid}
+    end
+
+    context "an invalid ref that isn't a url" do
+      let(:a) { ATDIS::Authority.new(ref: "foobar")}
+      it {
+        a.should_not be_valid
+        a.errors.messages.should == {ref: [
+          ATDIS::ErrorMessage.new("is not a valid URL", "4.2"),
+          ATDIS::ErrorMessage.new("is not a valid Unique Authority Identifier", "4.2")
+        ]}
+      }
+    end
+
+    context "an invalid ref because it doesn't end in atdis/1.0" do
+      let(:a) { ATDIS::Authority.new(ref: "http://www.council.nsw.gov.au/foobar")}
+      it {
+        a.should_not be_valid
+        a.errors.messages.should == {ref: [ATDIS::ErrorMessage.new("is not a valid Unique Authority Identifier", "4.2")]}
+      }
+    end
+  end
+end
