379fafb930870f79e8dd3a65ca32291c2fa0f3a0
Matthew Landauer <matthew@oaf.org.au>
2014-07-02 13:53:40 +1000

    Rspec 3 uses be_truthy rather be_true

--- spec/atdis/models/page_spec.rb
+++ spec/atdis/models/page_spec.rb
@@ -131,14 +131,14 @@ describe ATDIS::Models::Page do
 
       it do
         page.should_not be_valid
-        page.errors.messages.has_key?(:json).should be_true
+        page.errors.messages.has_key?(:json).should be_truthy
         page.errors.messages.count.should == 1
         # The error messages returned by the library are different for different Ruby versions
         ruby19_message = ATDIS::ErrorMessage["Invalid JSON: 784: unexpected token at '{\n  \"response\": [\n    {\n      \"application\": {\n        \"description\": \"application2\"\n      }\n    }\n  ],\n}\n'", nil]
         ruby20_message = ATDIS::ErrorMessage["Invalid JSON: 795: unexpected token at '{\n  \"response\": [\n    {\n      \"application\": {\n        \"description\": \"application2\"\n      }\n    }\n  ],\n}\n'", nil]
         page.errors.messages[:json].count.should == 1
         message = page.errors.messages[:json].first
-        (message == ruby19_message || message == ruby20_message).should be_true
+        (message == ruby19_message || message == ruby20_message).should be_truthy
       end
     end
 
